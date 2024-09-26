1d80edb1b9b7a5fbdac3066d972636a57f504bd1
Administrator <2814126219@qq.com>
2024-09-26 16:19:39 +0800

    fix:修复课表头的布局

--- src/components/timeTable/tablePage.tsx
+++ src/components/timeTable/tablePage.tsx
@@ -24,29 +24,44 @@ export default function TablePage(): JSX.Element {
             >
                 <View style={styleSheet.guideBar}>
                     <Image
-                        source={require('../../assets/svg/leftArrow.png')}
+                        source={require('../../assets/png/leftArrow.png')}
                         style={{
                             width: 30,
                             height: 30,
+                            position: 'absolute',
+                            left: 5,
+                            bottom: '40%',
                         }}></Image>
                     <View style={styleSheet.weekBox}>
                         <View style={styleSheet.textBox}>
-                            <Text style={{color: fontColor}}>123</Text>
-                            <Text style={{color: fontColor}}>456</Text>
+                            <Text style={{color: fontColor, height: 20, lineHeight: 20, fontSize: 18, fontWeight: '600'}}>第5周</Text>
+                            <Text style={{color: fontColor, fontSize: 12}}>本周</Text>
                             <Image
-                                source={require('../../assets/svg/leftArrow.png')}
+                                source={require('../../assets/png/leftArrow.png')}
                                 style={{
-                                    width: 15,
-                                    height: 15,
+                                    width: 18,
+                                    height: 18,
                                     transform: [
                                         {rotate: '-90deg'}
                                     ],
                                     position: 'absolute',
-                                    right: -15,
+                                    right: -20,
+                                    top: 0,
                                 }}
                             ></Image>
                         </View>
                     </View>
+
+                    <Image
+                        source={require('../../assets/png/reload.png')}
+                        style={{
+                            width: 26,
+                            height: 26,
+                            position: 'absolute',
+                            right: 10,
+                            bottom: '40%'
+                        }}
+                    ></Image>
                 </View>
             </ScrollView>
         </fontContext.Provider>
@@ -55,21 +70,31 @@ export default function TablePage(): JSX.Element {
 
 const styleSheet = StyleSheet.create({
     guideBar: {
-        paddingBottom: 16,
+        paddingTop: 50,
         justifyContent: 'flex-start',
-        alignItems: 'flex-end',
         flexDirection: 'row',
         width: '100%',
         height: 100,
-        backgroundColor: '#ff6275'
+        backgroundColor: '#ff6275',
+        position: 'relative',
     },
 
     weekBox: {
         width: '30%',
+        height: '100%',
+        position: 'absolute',
+        left: '35%',
+        bottom: '10%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
 
     textBox: {
         maxWidth: 50,
         position: 'relative',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
     }
 })
